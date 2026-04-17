dffa277e62cd074b9fe124bc5ea330db30fa2a1c
kalai-vani-p <kalaivani.deve@gmail.com>
2026-04-17 10:40:44 +0530

    Readme File updated

--- src/components/CommonTable.js
+++ src/components/CommonTable.js
@@ -38,6 +38,7 @@ const CommonTable = ({ tabs }) => {
       from: dayjs().subtract(2, "month").startOf("month").format("YYYY-MM-DD"),
       to: dayjs().format("YYYY-MM-DD"),
     }
+    
   });
 
   const debouncedSearch = useDebounce(tableState.search, 300);
